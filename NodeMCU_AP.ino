f83f978edc6115973667c616b76e8d263b929596
valogan <59672391+valogan@users.noreply.github.com>
2022-10-13 10:08:23 -0400

    Update NodeMCU_AP.ino
    Some UI networking changes

--- NodeMCU_AP.ino
+++ NodeMCU_AP.ino
@@ -1,5 +1,12 @@
+/* to do:
+1. figure out a way to get the IP address for the gateway NodeMCU here
+2. User Interface code
+*/
+
 #include <ESP8266WiFi.h>
 #include "ESPAsyncWebServer.h"
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 String readConcentration() { // returns alcohol concentration as a string
   return "0.02%";
@@ -13,7 +20,30 @@ String readHumidity() { // returns humidity as a string
   return "30 humidities";
 }
 
+String UI(String gatewayIP) {/* Create webpage for UI here and return as a string */
+  WiFiClient client;
+  HTTPClient http;
+  String controls;
+  
+  conPath = gatewayIP + "/controls";
+  http.begin(client, conPath.c_str());
+  http.GET(); // this will send the environmental controls status request to the station NodeMCU
+  controls = http.getString();
+}
+
+String manOverride(String gatewayIP) {/* Sends override to other server, then returns a confirmation or a failure notice to the UI */
+  WiFiClient client;
+  HTTPClient http;
+  
+  conPath = gatewayIP + "/manOverride";
+  http.begin(client, conPath.c_str());
+  http.GET(); // this will send the manual override to the station NodeMCU
+
+  return http.getString(); // confirmation or error for manual override will be returned to the UI
+}
+
 AsyncWebServer sensorServer(80); // sensor server on port 80 (HTTP)
+
 
 void setup() {
   Serial.begin(9600);
@@ -23,7 +53,7 @@ void setup() {
   *************************/
   
   Serial.print("Starting WiFi network...");
-  const char* ssid     = "DWAVES";
+  const char* ssid     = "Eni";
   const char* password = "123456789";
 
   WiFi.softAP(ssid, password); // start WiFi network
@@ -51,6 +81,14 @@ void setup() {
 
   sensorServer.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){ // web request for humidity
   request->send_P(200, "text/plain", readHumidity().c_str());
+  });
+
+  sensorServer.on("/UI", HTTP_GET, [](AsyncWebServerRequest *request){ // web request for the UI display // the object name should potentially just be "/"
+  request->send_P(200, "text/plain", UI(gatewayIP).c_str());
+  });
+
+  sensorServer.on("/UI/override", HTTP_GET, [](AsyncWebServerRequest *request){ // web request for the manual override
+  request->send_P(200, "text/plain", manOverride(gatewayIP).c_str());
   });
 
   sensorServer.begin();
