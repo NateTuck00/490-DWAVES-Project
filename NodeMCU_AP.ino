b2f6de706807f54971391417e87b6f5d6561dca9
valogan <59672391+valogan@users.noreply.github.com>
2022-10-16 19:46:37 -0400

    Added real sensor values

--- NodeMCU_AP.ino
+++ NodeMCU_AP.ino
@@ -1,6 +1,6 @@
 /* to do:
-1. Replace sensor dummy variables with real readings
-2. User Interface code
+1. User Interface code
+2. Do some clean-up
 */
 
 #include <ESP8266WiFi.h>
@@ -9,27 +9,62 @@
 #include <WiFiClient.h>
 #include "Zanshin_BME680.h"
 
-String stationIP = "http://192.168.1.22"; // ip of the gateway server
+String stationIP = "http://192.168.1.22"; // ip of the station server
 
 int i = 3000000; // iterate in loop() // basically a timer // initially set high so that get request in loop is made first
 String controlsState; // global variable for state of the environtal controls
 int overrideRequest = 0; // flag is set to 1 to override the system
 String manOverrideState; //not really needed... but we'll see what I do with it
 
+/*************************
+|      Sensor Setup      |
+*************************/
+// source: https://github.com/Zanduino/BME680/blob/master/examples/I2CDemo/I2CDemo.ino
+
+BME680_Class BME680; // BME680 Object
+static int32_t  temp, humidity, pressure, gas; // readings from BME680
+
+float altitude(const int32_t press, const float seaLevel = 1013.25);
+float altitude(const int32_t press, const float seaLevel) {
+  /*!
+  @brief     This converts a pressure measurement into a height in meters
+  @details   The corrected sea-level pressure can be passed into the function if it is known,
+             otherwise the standard atmospheric pressure of 1013.25hPa is used (see
+             https://en.wikipedia.org/wiki/Atmospheric_pressure) for details.
+  @param[in] press    Pressure reading from BME680
+  @param[in] seaLevel Sea-Level pressure in millibars
+  @return    floating point altitude in meters.
+  */
+  static float Altitude;
+  Altitude =
+      44330.0 * (1.0 - pow(((float)press / 100.0) / seaLevel, 0.1903));  // Convert into meters
+  return (Altitude);
+}  // of method altitude()
+
+
 String readConcentration() { // returns alcohol concentration as a string
-  return "0.02%";
+  BME680.getSensorData(temp, humidity, pressure, gas);
+  char buf[16];
+  sprintf(buf, "%d.%d", (int8_t)(gas / 100), (uint8_t)(gas % 100));
+  return buf;
 }
 
 String readTemperature() { // returns the temperature as a string
-  return "20 F";
+  BME680.getSensorData(temp, humidity, pressure, gas);
+  char buf[16];
+  sprintf(buf, "%d.%d", (int8_t)(temp / 100), (uint8_t)(temp % 100));
+  return buf;
 }
 
 String readHumidity() { // returns humidity as a string
-  return "30 humidities";
+  BME680.getSensorData(temp, humidity, pressure, gas);
+  char buf[16];
+  sprintf(buf, "%d.%d", (int8_t)(humidity / 1000), (uint8_t)(humidity % 1000));
+  return buf;
 }
 
 String UI() {/* Create webpage for UI here and return as a string */
-  return controlsState;  
+  return controlsState;
 }
 
 AsyncWebServer sensorServer(80); // sensor server on port 80 (HTTP)
@@ -40,6 +75,33 @@ IPAddress gateway(192, 168, 1, 1);
 
 void setup() {
   Serial.begin(9600);
+  // start sample code
+  /*!
+  @brief    Arduino method called once at startup to initialize the system
+  @details  This is an Arduino IDE method which is called first upon boot or restart. It is only
+            called one time and then control goes to the main "loop()" method, from which
+            control never returns
+  @return   void
+  */
+  Serial.begin(9600);  // Start serial port at Baud rate
+#ifdef __AVR_ATmega32U4__      // If this is a 32U4 processor, then wait 3 seconds to init USB port
+  delay(3000);
+#endif
+  Serial.print(F("Starting I2CDemo example program for BME680\n"));
+  Serial.print(F("- Initializing BME680 sensor\n"));
+  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
+    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
+    delay(5000);
+  }  // of loop until device is located
+  Serial.print(F("- Setting 16x oversampling for all sensors\n"));
+  BME680.setOversampling(TemperatureSensor, Oversample16);  // Use enumerated type values
+  BME680.setOversampling(HumiditySensor, Oversample16);     // Use enumerated type values
+  BME680.setOversampling(PressureSensor, Oversample16);     // Use enumerated type values
+  Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
+  BME680.setIIRFilter(IIR4);  // Use enumerated type values
+  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));  // "�C" symbols
+  BME680.setGas(320, 150);  // 320�c for 150 milliseconds
+  // end sample code
 
   /*************************
   | Setup for WiFi Network |
@@ -53,7 +115,6 @@ void setup() {
   WiFi.softAP(ssid, password); // start WiFi network
   Serial.println("Done!");
 
-  
   IPAddress IP = WiFi.softAPIP(); // The IP address
   Serial.print("Access Point SSID: ");
   Serial.println(ssid);
@@ -63,8 +124,6 @@ void setup() {
   /**************************
   | Setup for Sensor Server |
   **************************/
-
-  
 
   Serial.print("Starting server...");
 
@@ -118,8 +177,7 @@ void loop() {
     String conPath = stationIP + "/controls";
     http.begin(client, conPath.c_str());
     int getState = http.GET();
-    Serial.print("http.GET() returned: ");
-    Serial.println(getState);
+    Serial.print("http.GET() returned: "); Serial.println(getState);
 
     controlsState = http.getString(); // this will be the state of the controls. It's returned to the global controlState variable to be used in the UI function
     //Serial.println(controlsState);
