357c2cadf1127ceee1f960e94e666facfd890b32
NateTuck00 <86788124+NateTuck00@users.noreply.github.com>
2022-11-19 14:42:36 -0500

    Swapped fan timer back to timer 2
    vent pin is now 3

--- arduinomegafinalcode.ino
+++ arduinomegafinalcode.ino
@@ -6,7 +6,7 @@ File myFile;
 String dataString = ""; 
 
 Servo myservo;
-word VentPin = 45;
+word VentPin = 3;
 bool substate= false;
 
 
@@ -160,7 +160,7 @@ State 2: Fans low louvers low
 
 else if(concentration < 200){
   
-  pwmDuty(160);
+  pwmDuty(20);
   pos=80;
  
   Serial.println("State 2");
@@ -172,7 +172,7 @@ State3: Fans medium low louvers medium low
 
 else if(concentration < 400){
   
-  pwmDuty(320);
+  pwmDuty(40);
   pos=105;
   
   
@@ -185,7 +185,7 @@ State4: Fans medium high louvers medium high
 
 else if(concentration < 800){
   
-  pwmDuty(480);
+  pwmDuty(60);
   pos=130;
   
   Serial.println("State 4");
@@ -197,7 +197,7 @@ State5: Wide open
 
 else if(concentration >= 800){
   
-  pwmDuty(639);
+  pwmDuty(79);
   pos=155;
   
   Serial.println("State 5");
@@ -246,14 +246,17 @@ oldpos=pos;
 
 
 void pwm25kHzBegin() {
-  TCCR5A = B00100011; // Fast PWM
-  TCCR5B = B11001; // Prescaler = 1
-  OCR5A = 639; // TOP
-  //pinMode(44, OUTPUT);
-  //pinMode(45, OUTPUT);      //we only use 45
-  //pinMode(46, OUTPUT);
+  TCCR2A = 0;                               // TC2 Control Register A
+  TCCR2B = 0;                               // TC2 Control Register B
+  TIMSK2 = 0;                               // TC2 Interrupt Mask Register
+  TIFR2 = 0;                                // TC2 Interrupt Flag Register
+  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);  // OC2B cleared/set on match when up/down counting, fast PWM
+  TCCR2B |= (1 << WGM22) | (1 << CS21);     // prescaler 8
+  OCR2A = 79;                               // TOP overflow value (Hz)
+  OCR2B = 0;
 }
 
 void pwmDuty(byte ocrb) {
-  OCR5A = ocrb;                             // PWM Width (duty)
+  OCR2B = ocrb;                             // PWM Width (duty)
 }
+
